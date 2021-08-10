3d4ee8b6849ffc9379ecb3ccab58cb25e6bce704
Michal <rafaszromek@gmail.com>
2021-08-10 21:35:11 +0200

    git commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+const axios = require('axios').default;
 
+function dane()
+{
+  return axios.get('https://content.guardianapis.com/search?&q=debate&api-key=86069387-0efe-47f5-a62d-bc65c5e9e7db');
+  
+  /*.then(resp => {
+
+    let prz = '<p>{resp}</p>';
+    return prz;
+  });*/
+}
+
+
+
+/*axios.get('https://content.guardianapis.com/search?&q=debate&api-key=86069387-0efe-47f5-a62d-bc65c5e9e7db').then(resp => {
+
+    console.log(resp.data.response.results);
+});*/
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>ta</p>
       </header>
     </div>
   );
